216ba7c1c5c33a7cefd3e403a4a314704504af05
VilkhovyiDmytro <dmitrovilhovij@gmail.com>
2024-03-07 11:49:53 +0200

    finish

--- myApp.js
+++ myApp.js
@@ -1,39 +1,14 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+app.get("/api/:date", (req, res) => {
+  const date = Number(req.params.date)
+    ? new Date(+req.params.date)
+    : new Date(req.params.date);
+  res.status(200).json({
+    unix: Date.parse(date),
+    utc: date.toUTCString(),
+  });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
